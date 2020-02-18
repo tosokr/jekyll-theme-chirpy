8626bb07bc63b4ee0066e2c58351b6b830a315a4
Aleksandar Stefanov <aleksandar.stefanov@devoteam.nl>
2020-02-18 20:57:01 +0100

    Add tests to the pipeline script

--- tools/azuredevopsbuild.sh
+++ tools/azuredevopsbuild.sh
@@ -5,3 +5,11 @@ touch Gemfile.lock
 chmod a+w Gemfile.lock
 bundle config disable_platform_warnings true
 jekyll build --trace
+
+#Execute tests
+bundle exec htmlproofer _site \
+  --disable-external \
+  --check-html \
+  --empty_alt_ignore \
+  --allow_hash_href \
+  --url_ignore cdn.jsdelivr.net
